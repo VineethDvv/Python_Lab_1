32e26eda19fa4c74ef8389574a782ba60270bb1a
VineethDvv <vd3d3@mail.umkc.edu>
2019-02-10 19:50:07 -0600

    Update Task_2

--- Lab 1/task_2.py
+++ Lab 1/task_2.py
@@ -3,28 +3,39 @@ def Convert(tup, di):
         di.setdefault(a, []).append(b)
     return di
 
+# count = input("\nEnter number of tuples to be enterted\t-")
+# noTup = int(count)
 
-tp1 = ('John', ('Physics', 80))
-tp2 = ('Daniel', ('Science', 90))
-tp4 = ('John', ('Chemistry', 60))
 
-count = input("\nEnter number of tuples to be enterted\t-")
-noTup = int(count)
+# for i in range(noTup):
+#     name = input("\nEnter the name\t-")
+#     subject = input("\nEnter the subject\t-")
+#     marks = input("\nEnter the marks\t-")
+#     tp = (name, (subject), int(marks))
+#     lt.append(tp)
+
 
 lt = []
+tp1 = ('John', ('Physics', 80))
+tp2 = ('Daniel', ('Science', 90))
+tp3 = ('John', ('Chemistry', 60))
+tp4 = ('Mark',('Maths',100))
+tp5 = ('Daniel',('History',75))
+tp6 = ('Mark',('Social', 95))
 
-lt = [tp1, tp2, tp4]
-print(lt)
+
+lt1 = [tp1,tp2,tp3,tp4,tp5,tp6]
+
+print(f"Tuple Form - {lt1}")
 
 dictionary = {}
-print(Convert(lt,dictionary))
+dict1 = Convert(lt1,dictionary)
 
-for i in range(noTup):
-    name = input("\nEnter the name\t-")
-    subject = input("\nEnter the subject\t-")
-    marks = input("\nEnter the marks\t-")
-    tp = (name, (subject), int(marks))
-    lt.append(tp)
+
+print(f"Dictionary Form - {dict1}")
+
+
+
 
 
 
