db74838abde0e52338df639cc89db5a4fbccb304
Abilash7 <46832142+Abilash7@users.noreply.github.com>
2019-02-13 18:58:53 -0600

    Update task_1.py

--- Lab 1/task_1.py
+++ Lab 1/task_1.py
@@ -1,18 +1,18 @@
 Total_Amount = 0
 Deposit = 0
 Withdraw = 0
-choice = 0
+Choice = 0
 
 #print(choice)
-while(choice != 4):
-    choice = input("Press\t1 for Deposit\n\t\t2 for Withdrawal\n\t\t3 to show Total Amount\n\t\t4 to Exit-\n")
-    if (int(choice) == 1):
+while(Choice != 4):
+    Choice = input("Press\t1 for Deposit\n\t\t2 for Withdrawal\n\t\t3 to show Total Amount\n\t\t4 to Exit-\n")
+    if (int(Choice) == 1):
         Deposit = input("Deposit-\n")
         Total_Amount = Total_Amount + int(Deposit)#adding deposit to total amount.
-    elif (int(choice) == 2):
+    elif (int(Choice) == 2):
         Withdraw = input("Withdraw-\n")
         Total_Amount = Total_Amount - int(Withdraw)#subtracting withdraw amount from total amount
-    elif (int(choice) == 3):
+    elif (int(Choice) == 3):
         print("Total Amount - ${0}".format(Total_Amount))#printing total amount
     else:
         break
